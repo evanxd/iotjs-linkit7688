13ad9dd5b62e61b88703872dc3c9928db723d9fd
Evan Tseng <evan@tseng.io>
2016-07-27 12:14:54 +0800

    Add all pins

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 'use strict';
 
 module.exports = {
+  // Pins on the left side.
+  p1: 43,
+  p8: 20,
+  p9: 21,
+  p10: 2,
+  p11: 3,
+  p12: 0,
+  p13: 1,
+  // Pins on the right side.
   p14: 37,
-  p15: 44
+  p15: 44,
+  p16: 46,
+  p17: 45,
+  p18: 13,
+  p19: 12,
+  p20: 5,
+  p21: 4,
+  p25: 6,
+  p26: 18,
+  p27: 19,
+  p28: 17,
+  p29: 16,
+  p30: 15,
+  p31: 14
 };
