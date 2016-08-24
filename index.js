14a99d373747f82fde38f21b6c063fb72c3c68fa
Evan Tseng <evan@tseng.io>
2016-08-24 10:24:49 +0800

    Add pins info

--- index.js
+++ index.js
@@ -2,27 +2,109 @@
 
 module.exports = {
   // Pins on the left side.
-  p1: 43,
-  p8: 20,
-  p9: 21,
-  p10: 2,
-  p11: 3,
-  p12: 0,
-  p13: 1,
+  p1: {
+    group: 'ephy',
+    gpio: 43
+  },
+  p8: {
+    group: 'uart2',
+    gpio: 20,
+    pwm: 2,
+    uart: 'tx'
+  },
+  p9: {
+    group: 'uart2',
+    gpio: 21,
+    pwm: 3,
+    uart: 'rx'
+  },
+  p10: {
+    group: 'i2s',
+    gpio: 2,
+    i2s: 'ws'
+  },
+  p11: {
+    group: 'i2s',
+    gpio: 3,
+    i2s: 'clk'
+  },
+  p12: {
+    group: 'i2s',
+    gpio: 0,
+    i2s: 'sdi'
+  },
+  p13: {
+    group: 'i2s',
+    gpio: 1,
+    i2s: 'sdo'
+  },
   // Pins on the right side.
-  p14: 37,
-  p15: 44,
-  p16: 46,
-  p17: 45,
-  p18: 13,
-  p19: 12,
-  p20: 5,
-  p21: 4,
-  p25: 6,
-  p26: 18,
-  p27: 19,
-  p28: 17,
-  p29: 16,
-  p30: 15,
-  p31: 14
+  p14: {
+    group: 'refclk',
+    gpio: 37
+  },
+  p15: {
+    group: 'wled',
+    gpio: 44
+  },
+  p16: {
+    group: 'uart1',
+    gpio: 46,
+    uart: 'rx'
+  },
+  p17: {
+    group: 'uart1',
+    gpio: 45,
+    uart: 'tx'
+  },
+  p18: {
+    group: 'uart0',
+    gpio: 13,
+    uart: 'rx'
+  },
+  p19: {
+    group: 'uart0',
+    gpio: 12,
+    uart: 'tx'
+  },
+  p20: {
+    group: 'i2c',
+    gpio: 5,
+    i2c: 'sd'
+  },
+  p21: {
+    group: 'i2c',
+    gpio: 4,
+    i2c: 'sclk'
+  },
+  p25: {
+    group: 'spi_cs1',
+    gpio: 6
+  },
+  p26: {
+    group: 'pwm0',
+    gpio: 18,
+    pwm: 0
+  },
+  p27: {
+    group: 'pwm1',
+    gpio: 19,
+    pwm: 1
+  },
+  p28: {
+    group: 'spi_s',
+    gpio: 17
+  },
+  p29: {
+    group: 'spi_s',
+    gpio: 16
+  },
+  p30: {
+    group: 'spi_s',
+    gpio: 15
+  },
+  p31: {
+    group: 'spi_s',
+    gpio: 14
+  }
 };
